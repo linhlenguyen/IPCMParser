2757452872283f2f6c70be59168ad653d6c71185
linhlenguyen <ngl.linh@gmail.com>
2016-11-29 11:38:26 +0000

    Handling xml special characters

--- Data/ExpExporter.hs
+++ Data/ExpExporter.hs
@@ -9,6 +9,12 @@ exportRules
     import Data.List
     import Data.Map.Strict
 
+    resolveInvalidXMLCharacters :: String -> String
+    resolveInvalidXMLCharacters str = reverse $ Prelude.foldl foldf [] str
+      where foldf s c = case c of { '&' -> ";pma&" ++ s;
+                                    '"' -> ";43#&";
+                                    a -> a:s; }
+
     exportXmlToString :: [IPCRuleXml] -> [String]
     exportXmlToString xml = Prelude.map toXML $ Data.List.sort xml
 
